2fbfba1291668e82e36b676e6ea71f6b0c373da9
Raniere Rocha <rlsmont123@hotmail.com>
2024-11-22 16:24:23 -0300

    Feat: Add vitest and testing-library and setup

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTest.ts'
+
   }
 })
